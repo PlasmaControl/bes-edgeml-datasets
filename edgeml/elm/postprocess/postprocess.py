ab6c7115894f70e3503ad5a9e8aee9efa3bf3be6
David Smith <drsmith@pppl.gov>
2021-07-16 13:23:42 -0700

    mods

--- edgeml/elm/postprocess/postprocess.py
+++ edgeml/elm/postprocess/postprocess.py
@@ -1,17 +1,31 @@
 from pathlib import Path
-import datetime
+from datetime import datetime
+import subprocess, glob, shlex
 
 import numpy as np
+import matplotlib.pyplot as plt
 import h5py
 
-from ...bes2hdf5 import traverse_h5py
+try:
+    from ...bes2hdf5 import traverse_h5py
+except:
+    from edgeml.bes2hdf5 import traverse_h5py
 
+data_dir = Path('data')
+figure_dir = Path('figures')
+for subdir in [data_dir, figure_dir]:
+    subdir.mkdir(exist_ok=True)
 
-data_dir = Path('data').absolute()
-data_dir.mkdir(exist_ok=True)
+# original labeled data
+original_data_dir = Path(
+    '/fusion/projects/diagnostics/bes/smithdr/labeled-elms/data')
+assert(original_data_dir.exists())
+original_data_files = list(original_data_dir.glob('*/labeled-elm-events*.hdf5'))
+assert(original_data_files)
 
-figure_dir = Path('figures').absolute()
-figure_dir.mkdir(exist_ok=True)
+# combined data file
+combined_data_file = data_dir / 'labeled-elm-events.hdf5'
+
 
 
 def ensure_unique(array):
@@ -24,75 +38,265 @@ def ensure_unique(array):
 
 
 def combine_labeled_data_files():
-    # original labeled data
-    original_data_dir = Path('/fusion/projects/diagnostics/bes/smithdr/labeled-elms/data')
-    original_data_files = list(original_data_dir.glob('*/labeled-elm-events*.hdf5'))
-    # new combined data file
-    combined_data_file = data_dir / 'labeled-elm-events.hdf5'
-    # rename if exists
+    # rename combined data file if exists
     if combined_data_file.exists():
-        new_filename = f"labeled-elm-events-{datetime.strftime('%Y-%m-%d-%H-%M-%S')}.hdf5"
+        new_filename = f"labeled-elm-events-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.hdf5"
         print(f'Renaming old data file: {new_filename}')
         combined_data_file.rename(data_dir / new_filename)
     assert(combined_data_file.exists() is False)
     # create new combined data file
     with h5py.File(combined_data_file, 'w') as combined_data:
-        for original_data_file in original_data_files:
+        for ifile, original_data_file in enumerate(original_data_files):
             print(original_data_file)
             traverse_h5py(original_data_file, skip_subgroups=True)
             with h5py.File(original_data_file, 'r') as original_data:
                 print(original_data.attrs['labeled_elms'].size, len(original_data))
                 # assert(original_data.attrs['labeled_elms'].size == len(original_data))
                 # assert(ensure_unique(original_data.attrs['labeled_elms']))
-
-
-def combine_datafiles(
-        files=(),
-        newfile='labeled-elm-events.hdf5',
-        ):
-    with h5py.File(newfile, 'w') as nf:
-        for i, filename in enumerate(files):
-            with h5py.File(filename, 'r') as f:
-                for attrname in f.attrs:
+                for attrname in original_data.attrs:
                     assert(attrname in ['labeled_elms', 'skipped_elms'])
-                    if i == 0:
-                        nf.attrs.create(attrname, f.attrs[attrname])
+                    if ifile == 0:
+                        combined_data.attrs.create(attrname, original_data.attrs[attrname])
                     else:
-                        nf.attrs[attrname] = np.append(nf.attrs[attrname], f.attrs[attrname])
-                for elm_key, elm_group in f.items():
+                        combined_data.attrs[attrname] = np.append(combined_data.attrs[attrname],
+                                                                  original_data.attrs[attrname])
+                for elm_key, elm_group in original_data.items():
                     assert(isinstance(elm_group, h5py.Group))
-                    new_elm_group = nf.create_group(elm_key)
+                    new_elm_group = combined_data.create_group(elm_key)
                     for ds_key, ds_value in elm_group.items():
                         assert(isinstance(ds_value, h5py.Dataset))
                         new_elm_group.create_dataset(ds_key, data=ds_value)
-                nf.attrs['labeled_elms'] = np.unique(nf.attrs['labeled_elms'])
-        labeled_elms = nf.attrs['labeled_elms']
+                    for attr_name, attr_value in elm_group.attrs.items():
+                        new_elm_group.attrs[attr_name] = attr_value
+                combined_data.attrs['labeled_elms'] = np.unique(combined_data.attrs['labeled_elms'])
+        labeled_elms = combined_data.attrs['labeled_elms']
         print(f'Size of `labeled_elms` array: {labeled_elms.size}')
-        print(f'Number of groups: {len(nf)}')
+        print(f'Number of groups: {len(combined_data)}')
         assert (ensure_unique(labeled_elms))
-        assert (labeled_elms.size == len(nf))
+        assert (labeled_elms.size == len(combined_data))
+    traverse_h5py(combined_data_file, skip_subgroups=True)
 
 
+def plot_pdfs(interactive=True):
+    assert(combined_data_file.exists())
+    if interactive:
+        plt.ion()
+    else:
+        plt.ioff()
+    downsample = 10
+    linewidth = 0.75
+    plt.figure(figsize=(10, 6))
+    with h5py.File(combined_data_file, 'r') as hfile:
+        i_page = 0
+        n_elms = len(hfile)
+        elms_per_page = 8
+        total_pages = n_elms // elms_per_page +1
+        for i_elm, group_name in enumerate(hfile):
+            i_elm_page = i_elm % elms_per_page
+            if i_elm_page == 0:
+                i_page += 1
+                print(f'Plotting page {i_page} of {total_pages}...')
+            elm_group = hfile[group_name]
+            time = np.array(elm_group['time'])
+            signals = np.array(elm_group['signals']) / 10
+            labels = np.array(elm_group['labels'])
+            ax1 = plt.subplot(4,4,2*i_elm_page+1)
+            plt.plot(time[::downsample], signals[[20,22,24],::downsample].T, color='C0', lw=linewidth)
+            plt.plot(time[::downsample], labels[::downsample], color='C1', lw=linewidth)
+            ax2 = plt.subplot(4,4,2*i_elm_page+2)
+            one_indices = np.nonzero(labels)[0]
+            i1 = np.max([one_indices[0] - one_indices.size//2, 0])
+            i2 = np.min([one_indices[-1] + one_indices.size//2, time.size])
+            plt.plot(time[i1:i2], signals[[20,22,24],i1:i2].T, color='C0', lw=linewidth)
+            plt.plot(time[i1:i2], labels[i1:i2], color='C1', lw=linewidth)
+            plt.annotate('zoomed',
+                         (0.78, 0.88),
+                         xycoords='axes fraction',
+                         fontsize='x-small',
+                         color='red')
+            for ax in [ax1, ax2]:
+                plt.sca(ax)
+                plt.xticks(size='small')
+                plt.yticks(size='small')
+                plt.annotate(f"Sh {elm_group.attrs['shot']}",
+                             (0.03,0.88),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+                plt.annotate(f'Idx {group_name}',
+                             (0.03,0.76),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+            if i_elm_page == elms_per_page-1 or i_elm == n_elms-1:
+                plt.tight_layout(pad=0.3)
+                if interactive:
+                    plt.waitforbuttonpress()
+                else:
+                    filepath = figure_dir / f'labeled_elms_p{i_page:03d}.pdf'
+                    plt.savefig(filepath,
+                                format='pdf',
+                                transparent=True)
+                plt.clf()
+    if not interactive:
+        print('Combining PDF files...')
+        input_pdf_list = sorted(glob.glob('figures/labeled_elms_p*.pdf'))
+        output_pdf = figure_dir / 'labeled_elms.pdf'
+        command = 'gs -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress ' + \
+                  f'-o {output_pdf.as_posix()} ' + \
+                  ' '.join(input_pdf_list)
+        result = subprocess.run(shlex.split(command))
+        result.check_returncode()
+        print('Finished')
 
-# original_data_dir = data_dir / 'original_data'
-# assert(original_data_dir.exists())
-#
-# print('Original files')
-# original_data_files = list(original_data_dir.glob('*.hdf5'))
-# total_elm_count = 0
-# for file in original_data_files:
-#     print(f'  {file.name}   size {file.stat().st_size/(1024**2):.1f} MB')
-#     with h5py.File(file, 'r') as h5file:
-#         for key, value in h5file.attrs.items():
-#             print(f'    {key} shape {value.size}')
-#         labeled_elms = h5file.attrs['labeled_elms']
-#         print(labeled_elms.size, len(h5file))
-#         # assert(ensure_unique(labeled_elms))
-#         # assert(labeled_elms.size == len(h5file))
-#         total_elm_count += len(h5file)
-#
-# print(f'Total ELM count: {total_elm_count}')
 
+def plot_pdfs_2(interactive=True):
+    assert(combined_data_file.exists())
+    if interactive:
+        plt.ion()
+    else:
+        plt.ioff()
+    linewidth = 0.75
+    downsample = 4
+    plt.figure(figsize=(10,7))
+    with h5py.File(combined_data_file, 'r') as hfile:
+        i_page = 0
+        n_elms = len(hfile)
+        elms_per_page = 4
+        total_pages = n_elms // elms_per_page +1
+        for i_elm, group_name in enumerate(hfile):
+            i_elm_page = i_elm % elms_per_page
+            if i_elm_page == 0:
+                i_page += 1
+                print(f'Plotting page {i_page} of {total_pages}...')
+            elm_group = hfile[group_name]
+            time = np.array(elm_group['time'])[::downsample]
+            signals = np.array(elm_group['signals'])[:, ::downsample] / 10
+            labels = np.array(elm_group['labels'])[::downsample]
+            assert(time.size == labels.size and time.size == signals.shape[1])
+            one_indices = np.nonzero(labels)[0]
+            i1 = np.max([one_indices[0] - one_indices.size, 0])
+            i2 = np.min([one_indices[-1] + one_indices.size, labels.size])
+            for i_plot in range(4):
+                plt.subplot(4, 4, 4*i_elm_page + i_plot + 1)
+                signals_16 = signals[np.arange(i_plot*16, (i_plot+1)*16), i1:i2]
+                time_mod = (time[i1:i2]-time[i1]) * 1e3
+                plt.plot(time_mod, signals_16.T, color='C0', lw=linewidth)
+                plt.plot(time_mod, labels[i1:i2], color='C1', lw=linewidth)
+                plt.xticks(size='small')
+                plt.yticks(size='small')
+                plt.xlabel('Time (micro-s)', size='small')
+                plt.annotate(f"Sh {elm_group.attrs['shot']}",
+                             (0.02,0.88),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+                plt.annotate(f"T0 {time[0]:.2f} ms",
+                             (0.02,0.76),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+                plt.annotate(f'Idx {group_name}',
+                             (0.75,0.88),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+                plt.annotate(f'Ch {i_plot*16+1}-{(i_plot+1)*16}',
+                             (0.75, 0.76),
+                             xycoords='axes fraction',
+                             fontsize='x-small')
+            if i_elm_page == elms_per_page-1 or i_elm == n_elms-1:
+                plt.tight_layout(pad=0.4)
+                if interactive:
+                    plt.waitforbuttonpress()
+                else:
+                    filepath = figure_dir / f'labeled_elms_all_chan_p{i_page:03d}.pdf'
+                    plt.savefig(filepath,
+                                format='pdf',
+                                transparent=True)
+                plt.clf()
+    if not interactive:
+        print('Combining PDF files...')
+        input_pdf_list = sorted(glob.glob('figures/labeled_elms_all_chan_p*.pdf'))
+        output_pdf = figure_dir / 'labeled_elms_all_chan.pdf'
+        command = 'gs -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress ' + \
+                  f'-o {output_pdf.as_posix()} ' + \
+                  ' '.join(input_pdf_list)
+        result = subprocess.run(shlex.split(command))
+        result.check_returncode()
+        print('Finished')
+
+
+def remove_labeled_elms(elm_index_list=()):
+    assert(isinstance(elm_index_list, (list, tuple)))
+    assert(len(original_data_files) == 3)
+    print(f'Trying to delete {len(elm_index_list)} labeled ELMs from HDF5 files')
+    with h5py.File(original_data_files[0], 'a') as h0, \
+        h5py.File(original_data_files[1], 'a') as h1, \
+        h5py.File(original_data_files[2], 'a') as h2:
+        for elm_index in elm_index_list:
+            assert(isinstance(elm_index, int))
+            elm_str = f"{elm_index:05d}"
+            for i_hfile, hfile in enumerate([h0, h1, h2]):
+                if elm_str in hfile:
+                    print(f'Found ELM {elm_str} in file:')
+                    print(f'  {hfile.filename}')
+                    response = input(f"  Permanently delete ELM {elm_str}? (y/n): ")
+                    if response == 'y':
+                        labeled_elms = hfile.attrs['labeled_elms']
+                        idx = np.nonzero(labeled_elms == elm_index)[0]
+                        labeled_elms = np.delete(labeled_elms, idx)
+                        hfile.attrs['labeled_elms'] = labeled_elms
+                        del hfile[elm_str]
+                        print(f'  ELM {elm_str} deleted')
+                    else:
+                        print(f'  Skipping ELM {elm_str}')
+                    break
+                else:
+                    if i_hfile == 2:
+                        print(f'  ELM {elm_str} not found!')
+
+
+# candidate_bad_elms = [
+#     # 1282,  # ok
+#     # 1738,  # ok
+#     # 1799,  # ok
+#     # 1852,  # ok
+#     # 1898,  # ok
+#     # 1900,  # ok
+#     # 2364,  # ok
+#     # 3172,  # ok
+#     3426,  # strange post-ELM waveforms
+#     3960,  # strange waveforms
+#     3556,  # all ampl. < 0.5
+#     # 5226,  # ok
+#     4096, 4102, 4116, 4136, 4176, 4188, 4194, 4204,
+#     4358, 4588, 5212, 5276, 5480,
+#     5546,  # strange, consider deleting
+#     5684, 5692, 5694, 5775,
+#     6130,  # strange, consider deleting
+#     6150, 6446,
+#     # 6566,  # ok
+#     7168,  # low amplitudes, odd waveforms
+#     8456,  # low ampl
+#     # 8468,  # low ampl, but other ELMs in shot ok
+#     # 8558,  # ok
+#     # 8590,  # ok
+#     8750,  # low amplt. consider deleting
+#     10226, # strange waveforms
+# ]
+
+candidate_bad_elms_2 = [
+    6, 12, 24, 26,
+    60, 70, 72, 142,
+    332,
+    342, 412,
+    672, 698,
+    726, 802, 824, 1076, 1127,
+    1222, 1310, 1306, 3615,
+    4046, 4212, 4514, 4894, 5494, 5818,
+    5994, 6230, 6686, 6798, 6799,
+    7936, 7966, 8274, 8432, 8711, 8590,
+    8906, 8934, 8950, 9575, 9838, 9894, 9898,
+]
 
 if __name__=='__main__':
-    combine_labeled_data_files()
+    # combine_labeled_data_files()
+    plt.close('all')
+    plot_pdfs_2(interactive=False)
+    # remove_labeled_elms(candidate_bad_elms_2)
---
+++ edgeml/qhmode/workflow/elms.py
@@ -0,0 +1,131 @@
+from pathlib import Path
+import csv
+import numpy as np
+from edgeml import bes2hdf5
+import h5py
+import time as timelib
+
+
+# establish directories and CSV files in `elms/data`
+data_directory = Path(__file__).parent / 'data'
+data_directory.mkdir(exist_ok=True)
+metadata_directory = data_directory / 'metadata'
+metadata_directory.mkdir(exist_ok=True)
+signals_directory = data_directory / 'signals-8x8-only'
+signals_directory.mkdir(exist_ok=True)
+unlabeled_elms_directory = data_directory / 'unlabeled-elm-events'
+unlabeled_elms_directory.mkdir(exist_ok=True)
+shot_list_file = data_directory / 'shotlist.csv'
+elm_list_file = data_directory / 'elm-list.csv'
+
+
+def package_metadata(max_shots=None):
+    print(f'Using shotlist {shot_list_file.as_posix()}')
+    assert(shot_list_file.exists())
+    shot_list = []
+    with shot_list_file.open() as csvfile:
+        reader = csv.DictReader(csvfile,
+                                fieldnames=None,
+                                skipinitialspace=True)
+        for irow, row in enumerate(reader):
+            shot_list.append(int(row['shot']))
+            if max_shots and irow > max_shots:
+                break
+    filename = data_directory / 'bes_metadata.hdf5'
+    bes2hdf5.package_bes(shots=shot_list,
+                         verbose=True,
+                         with_signals=False,
+                         filename=filename.as_posix())
+
+
+def package_signals_8x8_only(max_shots=None):
+    metadata_file = metadata_directory / 'bes_metadata.hdf5'
+    print(f'Using metadata in {metadata_file.as_posix()}')
+    assert(metadata_file.exists())
+    shot_list = bes2hdf5.make_8x8_sublist(
+            path=metadata_file,
+            upper_inboard_channel=56,
+            noplot=True)
+    if max_shots:
+        shot_list = shot_list[0:max_shots]
+    bes2hdf5.package_bes(shots=shot_list,
+                         verbose=True,
+                         with_signals=True)
+
+
+def package_unlabeled_elm_events(max_elms=None):
+    print(f'Using ELM list file {elm_list_file.as_posix()}')
+    assert(elm_list_file.exists())
+    elms = []
+    t1 = timelib.time()
+    with elm_list_file.open() as csvfile:
+        reader = csv.DictReader(csvfile,
+                                fieldnames=None,
+                                skipinitialspace=True)
+        for irow, row in enumerate(reader):
+            elms.append({'shot': int(row['shot']),
+                         'start_time': float(row['start_time']),
+                         'stop_time': float(row['stop_time'])})
+    unlabeled_elm_events_file = Path('elm-events.hdf5')  # local output file
+    metadata_file = signals_directory / 'bes_metadata.hdf5'
+    current_shot = 0
+    signal_group = None
+    failed_shots = []
+    with h5py.File(unlabeled_elm_events_file, 'w') as elm_file_group, \
+            h5py.File(metadata_file, 'r') as metafile:
+        for ielm, elm in enumerate(elms):
+            if max_elms and ielm > max_elms:
+                break
+            if elm['shot'] != current_shot:
+                signal_file = signals_directory / \
+                              f"bes_signals_{elm['shot']:d}.hdf5"
+                if not signal_file.exists():
+                    if elm['shot'] not in failed_shots:
+                        print(f'File {signal_file.as_posix()} does not exist !!!')
+                        failed_shots.append(elm['shot'])
+                    continue
+                current_shot = elm['shot']
+                if signal_group:
+                    signal_group.close()
+                print(f"Loading shot {elm['shot']}")
+                signal_group = h5py.File(signal_file, 'r')
+                pinj_time = metafile[f'{current_shot:d}']['pinj_time'][:]
+                pinj_15l = metafile[f'{current_shot:d}']['pinj_15l'][:]
+                pinj_15r = metafile[f'{current_shot:d}']['pinj_15r'][:]
+                signals = signal_group['signals'][:, :]
+                time = signal_group['time'][:]
+                print('  Finished loading')
+            dt = elm['stop_time'] - elm['start_time']
+            # skip ELM event if PING 15L/R too low/high
+            pinj_mask = np.logical_and(pinj_time >= (elm['start_time'] - dt),
+                                       pinj_time <= (elm['stop_time'] + dt))
+            if (np.max(pinj_15l[pinj_mask]) < 0.5e6) or \
+                    (np.max(pinj_15r[pinj_mask]) > 0.5e6):
+                print(f'Skipping ELM {ielm} with max pinj_15l {np.max(pinj_15l[pinj_mask])} ' + \
+                      f'and max pinj_15r {np.max(pinj_15r[pinj_mask])}')
+                continue
+            time_mask = np.logical_and(time >= (elm['start_time'] - dt),
+                                       time <= (elm['stop_time'] + dt))
+            print(f"  ielm {ielm} in shot {current_shot} at time {elm['start_time']:.2f} ms " + \
+                  f"with {np.count_nonzero(time_mask)} time points")
+            elm_event_group = elm_file_group.create_group(f'{ielm:05d}')
+            elm_event_group.attrs['shot'] = elm['shot']
+            elm_event_group.create_dataset('signals', data=signals[:, time_mask])
+            elm_event_group.create_dataset('time', data=time[time_mask])
+    if signal_group:
+        signal_group.close()
+    bes2hdf5.traverse_h5py(unlabeled_elm_events_file)
+    print('Failed shots:', failed_shots)
+    t2 = timelib.time()
+    dt = t2 - t1
+    print(f'Elapsed time: {int(dt) // 3600} hr {dt % 3600 / 60:.1f} min')
+
+
+# if __name__ == '__main__':
+    # shotlist = read_shotlist()
+    # package_shotlist_metadata(max_shots=4)
+    # bes2hdf5.print_metadata_summary('data/elm_metadata/bes_metadata.hdf5', only_8x8=True)
+    # shotlist = bes2hdf5.make_8x8_sublist(path='data/elm_metadata/bes_metadata.hdf5',
+    #                                      upper_inboard_channel=56)
+    # package_8x8_sublist()
+    # package_unlabeled_elm_events()
