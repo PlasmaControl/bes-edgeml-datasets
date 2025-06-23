8e6d51a1764f5511231adfc97aafce6f25e0437e
David R. Smith <david.smith@wisc.edu>
2025-06-23 07:06:50 -0400

    mods

--- model_trainer/scenarios.py
+++ model_trainer/scenarios.py
@@ -2,21 +2,36 @@ from main import main
 
 data_file='/Users/drsmith/Documents/repos/bes-ml-data/model_trainer/small_data_100.hdf5'
 # data_file='/global/homes/d/drsmith/ml/bes-edgeml-datasets/model_trainer/small_data_200.hdf5'
-num_workers = 1
-max_elms = 20
-max_epochs = 10
+
+num_workers = 4
+max_elms = None
+max_epochs = 80
 
 
 def scenario_128(
         batch_size=128,
         lr=1e-3,
 ):
+    feature_model_layers = (
+        {'out_channels': 2, 'kernel': (8, 1, 1), 'stride': (8, 1, 1), 'bias': False},
+        {'out_channels': 2, 'kernel': (1, 3, 3), 'stride': 1,         'bias': False},
+        {'out_channels': 2, 'kernel': (4, 1, 1), 'stride': (4, 1, 1), 'bias': False},
+        {'out_channels': 2, 'kernel': (1, 3, 3), 'stride': 1,         'bias': False},
+        {'out_channels': 4, 'kernel': (1, 3, 3), 'stride': 1,         'bias': False},
+    )
+    mlp_task_models=None
     main(
         signal_window_size=128,
-        gradient_clip_val=1,
-        gradient_clip_algorithm='value',
+        experiment_name='experiment_128_v1',
         no_bias=True,
         batch_norm=False,
+        use_wandb=True,
+        # skip_data=True,
+        # skip_train=True,
+        gradient_clip_val=1,
+        gradient_clip_algorithm='value',
+        feature_model_layers=feature_model_layers,
+        mlp_task_models=mlp_task_models,
         batch_size=batch_size,
         lr=lr,
         num_workers=num_workers,
@@ -26,4 +41,8 @@ def scenario_128(
     )
 
 if __name__=='__main__':
-    scenario_128()
+    # scenario_128()
+    for _ in range(3):
+        scenario_128(batch_size=64)
+        scenario_128(batch_size=128)
+        scenario_128(batch_size=256)
