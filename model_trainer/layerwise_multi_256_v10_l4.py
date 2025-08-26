ea6a7124839dbfbed3c3da33e2e18f06c3b964c6
David Smith <david.smith@wisc.edu>
2025-08-26 13:13:53 -0700

    mods

--- model_trainer/layerwise_multi_256_v10_l4.py
+++ model_trainer/layerwise_multi_256_v10_l4.py
@@ -1,56 +1,3 @@
-#!/usr/bin/env bash
-#SBATCH --account=m3586
-#SBATCH --constraint=gpu
-#SBATCH --mail-user=david.smith@wisc.edu
-#SBATCH --mail-type=ALL
-
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus-per-node=4
-#SBATCH --cpus-per-task=32
-
-#SBATCH --time=60
-#SBATCH --qos=regular
-
-#SBATCH --signal=SIGTERM@200
-
-#SBATCH --array=1-10%5
-
-module --redirect list
-which python
-
-echo Python executable: $(which python)
-echo
-echo Job name: $SLURM_JOB_NAME
-echo QOS: $SLURM_JOB_QOS
-echo Account: $SLURM_JOB_ACCOUNT
-echo Submit dir: $SLURM_SUBMIT_DIR
-echo
-echo Job array ID: $SLURM_ARRAY_JOB_ID
-echo Job ID: $SLURM_JOBID
-echo Job array task: $SLURM_ARRAY_TASK_ID
-echo Job array task count: $SLURM_ARRAY_TASK_COUNT
-echo
-echo Nodes: $SLURM_NNODES
-echo Head node: $SLURMD_NODENAME
-echo hostname $(hostname)
-echo Nodelist: $SLURM_NODELIST
-echo Tasks per node: $SLURM_NTASKS_PER_NODE
-echo GPUs per node: $SLURM_GPUS_PER_NODE
-
-if [[ -n $SLURM_ARRAY_JOB_ID ]]; then
-    export UNIQUE_IDENTIFIER=${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}
-else
-    export UNIQUE_IDENTIFIER=$SLURM_JOBID
-fi
-echo UNIQUE_IDENTIFIER: ${UNIQUE_IDENTIFIER}
-
-export WANDB__SERVICE_WAIT=500
-
-export rand=${RANDOM}
-echo Random number: ${rand}
-
-SCRIPT=$(cat << END
 import os
 from numpy import random
 from model_trainer.main_multitask_v2 import main
@@ -92,13 +39,13 @@ if __name__=='__main__':
             {'out_channels': 4, 'kernel': (1, 3, 3), 'stride': 1,         'bias': True},
         ),
         mlp_tasks={
-            'elm_class': [None,32,1],
-            'conf_onehot': [None,32,4],
+            'elm_class': [None,16,1],
+            'conf_onehot': [None,16,4],
         },
         monitor_metric='sum_loss/train',
         fir_bp=fir_bp,
         # training
-        max_epochs=300,
+        max_epochs=500,
         log_freq=100,
         lr=1e-2,
         lr_warmup_epochs=20,
@@ -117,23 +64,3 @@ if __name__=='__main__':
         backbone_unfreeze_at_epoch = 50,
         backbone_warmup_rate = 2,
     )
-END
-)
-
-echo Script:
-echo "${SCRIPT}"
-
-
-JOB_DIR=${HOME}/scratch-ml
-echo Job directory: ${JOB_DIR}
-
-cd $JOB_DIR || exit
-echo Current directory: ${PWD}
-
-START_TIME=$(date +%s)
-srun --unbuffered python -c "${SCRIPT}"
-EXIT_CODE=$?
-END_TIME=$(date +%s)
-echo Slurm elapsed time $(( (END_TIME - START_TIME)/60 )) min
-
-echo Exit code: ${EXIT_CODE}
