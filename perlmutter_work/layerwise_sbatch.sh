4fa3dd64b59365e6a62f699bc149ec77f209dd43
David Smith <david.smith@wisc.edu>
2025-10-12 10:39:04 -0700

    mods

--- perlmutter_work/layerwise_sbatch.sh
+++ perlmutter_work/layerwise_sbatch.sh
@@ -11,9 +11,9 @@
 
 #SBATCH --signal=SIGTERM@180
 
-#SBATCH --time=120
+#SBATCH --time=0-4
 #SBATCH --qos=regular
-#SBATCH --array=0-39%8
+#SBATCH --array=0-47%8
 
 module --redirect list
 which python
@@ -52,25 +52,28 @@ from model_trainer.main_multitask_v3 import main
 
 if __name__=='__main__':
 
+    task_id = int(os.getenv('SLURM_ARRAY_TASK_ID'))
     fir_choices = (
         (8, None),
         (12, None),
         (None, 100),
         (None, 200),
     )
-    task_id = int(os.getenv('SLURM_ARRAY_TASK_ID', None))
+    fir_bp = fir_choices[task_id%len(fir_choices)]
+    weight_decay_choices = (1e-4, 1e-5)
+    weight_decay = weight_decay_choices[(task_id//len(fir_choices))%len(weight_decay_choices)]
 
     main(
         # scenario
         signal_window_size=256,
         experiment_name='multi_256_v26',
-        trial_name_prefix=f'L3_T{task_id:02d}',
+        trial_name_prefix=f'L3_BS512_T{task_id:02d}',
         # data
         elm_data_file='/global/homes/d/drsmith/scratch-ml/data/elm_data.20240502.hdf5',
         confinement_data_file='/global/homes/d/drsmith/scratch-ml/data/confinement_data.20240112.hdf5',
-        max_elms=120,
-        max_confinement_event_length=int(30e3),
-        confinement_dataset_factor=0.4,
+        max_elms=240,
+        max_confinement_event_length=int(40e3),
+        confinement_dataset_factor=0.5,
         fraction_validation=0.125,
         fraction_test=0.125,
         num_workers=4,
@@ -85,15 +88,15 @@ if __name__=='__main__':
             'conf_onehot': [None, 32, 4],
         },
         monitor_metric='sum_score/train',
-        fir_bp=fir_choices[task_id%len(fir_choices)],
+        fir_bp=fir_bp,
         # training
-        max_epochs=150,
+        max_epochs=200,
         lr=1e-2,
         lr_warmup_epochs=10,
         lr_scheduler_patience=50,
         lr_scheduler_threshold=1e-2,
-        weight_decay=1e-5,
-        batch_size=256,
+        weight_decay=weight_decay,
+        batch_size=512,
         use_wandb=True,
         early_stopping_patience=125,
     )
